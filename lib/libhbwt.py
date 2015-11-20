477b96a67db98347f3e20c4e5943128fcc9c864b
alddiaz <aldodiaz64@gmail.com>
2015-11-20 17:55:07 -0200

    Update libhbwt.py

--- lib/libhbwt.py
+++ lib/libhbwt.py
@@ -6,69 +6,42 @@ import numpy as np
 import scipy.signal as signal
 from libfilt import* #Biblioteca de filtragem
 
-#Funcao que calcula a Transformada Wavelet
-#dada a resposta ao impulso dos filtros Wavelet H(z) e G(z)
-#e o nivel de profundidade da analise
 def wt(x, h, g, N):
-	#x: Sinal de entrada
-	#h: Filtro de escala
-	#g: Filtro Wavelet
-	#N: Niveis de profundidade na analise Wavelet
-
 	L = h.size-1
 	h = h/np.linalg.norm(h)
 	g = g/np.linalg.norm(g)
 	a = np.empty((N,),dtype=object)
 	b = np.empty((N,),dtype=object)
 
-	for k in xrange(0,N):    # Conta de 0 ate (M-1)
-		x = np.append(x,np.zeros(L)) # Zero-padding
-		bb = signal.lfilter(g, 1, x) # Filtragem g[n] wavelet
-		bb = bb[1::2]          # Decimacao fator 2 das amostras impares
-		b[k] = bb              # Coeficientes wavelet
-		x = signal.lfilter(h, 1, x) # Filtragem h[n] escala
-		x = x[1::2]          # Decimacao fator 2 das amostras impares
+	for k in xrange(0,N):
+		x = np.append(x,np.zeros(L))
+		bb = signal.lfilter(g, 1, x)
+		bb = bb[1::2]
+		b[k] = bb
+		x = signal.lfilter(h, 1, x)
+		x = x[1::2]
 	
-	a[k] = x                     # Coeficientes de escala
+	a[k] = x
 	
 	return a, b
 
-#Funcao que para calculo do banco de filtros CMFB
 def cmfb(x, P):
-	#x: Sinal de entrada
-	#P: Numero de bancos
-
-	k = np.arange(P) #Numero de canais do banco
-	n = np.arange(2*P) #Tamanho dos filtros em amostras
-	n.shape = (-1,1) #Fazendo 'n' um vetor coluna
-	hn = np.sin((k+.5)*np.pi/2/P) #Janela seno h[n]
-	hn = np.hstack((hn,hn[::-1])) #Condicao de simetria
-	hn.shape = (-1,1) #Fazendo 'hk' um vetor coluna
-	h = hn*np.ones(P) #Matriz de ordem 2PxP
-	argum = (2*n+P+1)*(2*k+1)*np.pi/4/P #Argumento do cosseno
+	k = np.arange(P)
+	n = np.arange(2*P)
+	n.shape = (-1,1)
+	hn = np.sin((k+.5)*np.pi/2/P)
+	hn = np.hstack((hn,hn[::-1]))
+	hn.shape = (-1,1)
+	h = hn*np.ones(P)
+	argum = (2*n+P+1)*(2*k+1)*np.pi/4/P
  	coss = np.cos(argum)
-	w = np.sqrt(2.0/P)*h*coss #Gera a matriz de filtragem CMFB
-				    #Operador (*) e o produto elemento a elemento
-	y = exp_fir_diz(x, w[::-1,:], 1, P) # Filtragem e decimacao
-	#w[::-1,:]: Banco de analise (Reversao no tempo do Banco de sintese, mas tem resposta em frequencia identica)
+	w = np.sqrt(2.0/P)*h*coss
+	y = exp_fir_diz(x, w[::-1,:], 1, P)
 
 	return y, w
 
-#Funcao que calcula a HBWT
 def hbwt(x, h, g, P, N):
-	# ENTRADAS:
-	#x: Sinal de entrada
-	#h: Filtro de analise passa-baixa (escala)
-	#g: Filtro de analise passa-alta (Wavelet)
-	#P: Periodo do sinal x em numero de amostras
-	#N: Niveis de profundidade na analise Wavelet
-	
-	# SAIDAS:
-	# a: matriz de coeficientes de escala
-	# b: matriz de coeficientes wavelet
-	# cmfb: matriz de filtragem CMFB
-	
-	[x, w] = cmfb(x, P) #Divisao em sub-bandas usando MDCT (DCT-IV)
+	[x, w] = cmfb(x, P)
 	a = np.empty((N,P),dtype=object)
 	b = np.empty((N,P),dtype=object)
 
